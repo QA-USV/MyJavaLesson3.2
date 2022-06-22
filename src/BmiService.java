e981ef715947e80b8aa971229115f4f52f876953
Sergei US <106164110+QA-USV@users.noreply.github.com>
2022-06-22 15:01:12 +0300

    First commit

---
+++ src/BmiService.java
@@ -0,0 +1,5 @@
+public class BmiService {
+    public double calculate(double h, double w) {
+        return w / (h * h);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double height = 1.87; //Укажите свой рост в метрах;
+        double weight = 78.8; //Укажите свой вес в кг;
+        double bmi = service.calculate(height, weight);
+        String str = String.format("Ваш Индекс массы тела (BMI) равен: %.2f. ", bmi);
+        System.out.println(str);
+    }
+}
